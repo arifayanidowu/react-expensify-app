befe4245bb37174420f74b5d3a10b990f9056762
stizzle123 <arifayanidowu2@yahoo.com>
2018-07-20 20:23:03 +0100

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,59 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        index: './src/app.jsx'
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    module: {
-        rules: [{
-            test: /\.jsx$/,
-            include: [path.resolve(__dirname, 'src')],
-            exclude: [
-                path.resolve(__dirname, 'node_modules'),
-                path.resolve(__dirname, 'bower_components')
-            ],
-            loader: 'babel-loader'
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
-    ],
-    resolve: {
-        extensions: ['.json', '.js', '.jsx', '.css']
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-};
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    return {
+        mode: 'production',
+        entry: {
+            index: './src/app.jsx'
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'public')
+        },
+        module: {
+            rules: [{
+                test: /\.jsx$/,
+                include: [path.resolve(__dirname, 'src')],
+                exclude: [
+                    path.resolve(__dirname, 'node_modules'),
+                    path.resolve(__dirname, 'bower_components')
+                ],
+                loader: 'babel-loader'
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ],
+        resolve: {
+            extensions: ['.json', '.js', '.jsx', '.css']
+        },
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+}
